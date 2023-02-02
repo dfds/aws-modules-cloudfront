d6c78007c99bc9baae83acd048535a411ab75387
Aleksandra Korczak <alekorc@dfds.com>
2023-02-02 16:23:13 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,238 @@
+resource "aws_cloudfront_distribution" "this" {
+  aliases = var.aliases
+  comment = var.comment
+
+  dynamic "custom_error_response" {
+    for_each = var.custom_error_response
+
+    content {
+      error_code            = custom_error_response.value.error_code
+      error_caching_min_ttl = lookup(custom_error_response.value, "error_caching_min_ttl", null)
+      response_code         = lookup(custom_error_response.value, "response_code", "")
+      response_page_path    = lookup(custom_error_response.value, "response_page_path", "")
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods           = var.allowed_methods
+    cached_methods            = var.cached_methods
+    cache_policy_id           = var.cache_policy_id
+    compress                  = var.compress
+    field_level_encryption_id = var.field_level_encryption_id
+
+    dynamic "forwarded_values" {
+      for_each = length(var.forwarded_values) > 0 ? ["OK"] : []
+      content {
+        query_string            = lookup(var.forwarded_values, "query_string", false)
+        headers                 = lookup(var.forwarded_values, "headers", [])
+        query_string_cache_keys = lookup(var.forwarded_values, "query_string_cache_keys", [])
+        cookies {
+          forward           = lookup(var.forwarded_values.cookies, "forward", "none")
+          whitelisted_names = lookup(var.forwarded_values.cookies, "whitelisted_names", [])
+        }
+      }
+    }
+
+    dynamic "lambda_function_association" {
+      for_each = var.lambda_function_association
+      iterator = i
+
+      content {
+        event_type   = i.value.event_type
+        lambda_arn   = i.value.lambda_arn
+        include_body = i.value.include_body
+      }
+    }
+
+    dynamic "function_association" {
+      for_each = var.function_association
+      iterator = i
+
+      content {
+        event_type   = i.value.event_type
+        function_arn = i.value.function_arn
+      }
+    }
+
+    realtime_log_config_arn    = var.realtime_log_config_arn
+    response_headers_policy_id = var.response_headers_policy_id
+    target_origin_id           = var.target_origin_id
+    trusted_key_groups         = var.trusted_key_groups
+    trusted_signers            = var.trusted_signers
+    viewer_protocol_policy     = var.viewer_protocol_policy
+
+    min_ttl     = var.min_ttl
+    default_ttl = var.default_ttl
+    max_ttl     = var.max_ttl
+  }
+
+  default_root_object = var.default_root_object
+  enabled             = var.enabled
+  is_ipv6_enabled     = var.is_ipv6_enabled
+
+  dynamic "logging_config" {
+    for_each = length(var.logging_config) != 0 ? [var.logging_config] : []
+
+    content {
+      bucket          = logging_config.value.bucket
+      include_cookies = lookup(logging_config.value, "include_cookies", null)
+      prefix          = lookup(logging_config.value, "prefix", "")
+    }
+  }
+
+  dynamic "ordered_cache_behavior" {
+    for_each = length(var.ordered_cache_behavior) != 0 ? [var.ordered_cache_behavior] : []
+    iterator = i
+
+    content {
+      allowed_methods           = i.value.allowed_methods
+      cached_methods            = i.value.cached_methods
+      cache_policy_id           = lookup(i.value, "cache_policy_id", "")
+      compress                  = lookup(i.value, "compress", false)
+      default_ttl               = lookup(i.value, "default_ttl", null)
+      field_level_encryption_id = lookup(i.value, "field_level_encryption_id", "")
+
+      dynamic "forwarded_values" {
+        for_each = contains(i.value, "forwarded_values") ? [i.value.forwarded_values] : []
+        iterator = l
+
+        content {
+          cookies {
+            forward           = l.value.cookies.forward
+            whitelisted_names = lookup(l.value.cookies, "whitelisted_names", [])
+          }
+          headers                 = lookup(l.value, "headers", [])
+          query_string            = l.value.query_string
+          query_string_cache_keys = lookup(l.value, "query_string_cache_keys", [])
+        }
+      }
+
+      dynamic "lambda_function_association" {
+        for_each = contains(i.value, "lambda_function_association") ? [i.value.lambda_function_association] : []
+        iterator = l
+
+        content {
+          event_type   = l.value.event_type
+          lambda_arn   = l.value.lambda_arn
+          include_body = lookup(l.value, "include_body", false)
+        }
+      }
+
+      dynamic "function_association" {
+        for_each = contains(i.value, "function_association") ? [i.value.function_association] : []
+        iterator = f
+
+        content {
+          event_type   = f.value.event_type
+          function_arn = f.value.function_arn
+        }
+      }
+
+      max_ttl                    = lookup(i.value, "max_ttl", null)
+      min_ttl                    = lookup(i.value, "min_ttl", 0)
+      origin_request_policy_id   = lookup(i.value, "")
+      path_pattern               = i.value.path_pattern
+      realtime_log_config_arn    = lookup(i.value, "realtime_log_config_arn", "")
+      response_headers_policy_id = lookup(i.value, "response_headers_policy_id", "")
+      smooth_streaming           = lookup(i.value, "smooth_streaming", null)
+      target_origin_id           = i.value.target_origin_id
+      trusted_key_groups         = lookup(i.value, "trusted_key_groups", [])
+      trusted_signers            = lookup(i.value, "trusted_signers", [])
+      viewer_protocol_policy     = i.value.viewer_protocol_policy
+    }
+  }
+
+  dynamic "origin" {
+    for_each = var.origin
+
+    content {
+      domain_name         = origin.value.domain_name
+      origin_id           = origin.value.origin_id
+      connection_attempts = lookup(origin.value, "connection_attempts", null)
+      connection_timeout  = lookup(origin.value, "connection_timeout", null)
+
+      dynamic "custom_header" {
+        for_each = lookup(origin.value, "custom_header", [])
+
+        content {
+          name  = custom_header.value.name
+          value = custom_header.value.value
+        }
+      }
+      dynamic "custom_origin_config" {
+        for_each = contains(keys(origin.value), "custom_origin_config") ? [lookup(origin.value, "custom_origin_config")] : []
+
+        content {
+          http_port                = custom_origin_config.value.http_port
+          https_port               = custom_origin_config.value.https_port
+          origin_protocol_policy   = custom_origin_config.value.origin_protocol_policy
+          origin_ssl_protocols     = custom_origin_config.value.origin_ssl_protocols
+          origin_keepalive_timeout = lookup(custom_origin_config.value, "origin_keepalive_timeout", null)
+          origin_read_timeout      = lookup(custom_origin_config.value, "origin_read_timeout", null)
+        }
+      }
+
+      origin_access_control_id = lookup(origin.value, "origin_access_control_id", "")
+      origin_path              = lookup(origin.value, "origin_path", "")
+
+      dynamic "origin_shield" {
+        for_each = contains(keys(origin.value), "origin_shield") ? [lookup(origin.value, "origin_shield")] : []
+
+        content {
+          enabled              = origin_shield.value.enabled
+          origin_shield_region = origin_shield.value.origin_shield_region
+        }
+      }
+
+      dynamic "s3_origin_config" {
+        for_each = contains(keys(origin.value), "s3_origin_config") ? [lookup(origin.value, "s3_origin_config")] : []
+
+        content {
+          origin_access_identity = s3_origin_config.value.origin_access_identity
+        }
+      }
+    }
+  }
+
+  dynamic "origin_group" {
+    for_each = length(var.origin_group) != 0 ? [var.origin_group] : []
+
+    content {
+      origin_id = origin_group.value.origin_id
+
+      failover_criteria {
+        status_codes = origin_group.value.failover_criteria.status_codes
+      }
+
+      dynamic "member" {
+        for_each = origin_group.value.member
+
+        content {
+          origin_id = origin_group.value.member.origin_id
+        }
+      }
+    }
+  }
+
+  price_class = var.price_class
+
+  restrictions {
+    geo_restriction {
+      restriction_type = var.restrictions.geo_restriction.restriction_type
+      locations        = var.restrictions.geo_restriction.locations
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn            = lookup(var.viewer_certificate, "acm_certificate_arn", "")
+    cloudfront_default_certificate = lookup(var.viewer_certificate, "cloudfront_default_certificate", false)
+    iam_certificate_id             = lookup(var.viewer_certificate, "iam_certificate_id", "")
+    minimum_protocol_version       = lookup(var.viewer_certificate, "minimum_protocol_version", "TLSv1.2_2021")
+    ssl_support_method             = lookup(var.viewer_certificate, "ssl_support_method", "")
+  }
+
+  web_acl_id          = var.web_acl_id
+  retain_on_delete    = var.retain_on_delete
+  wait_for_deployment = var.wait_for_deployment
+
+  tags = var.cloudfront_tags
+}
---
+++ variables.tf
@@ -0,0 +1,290 @@
+variable "aliases" {
+  type        = list(string)
+  description = "List of extra CNAMEs (alternate domain names), if any, for this distribution"
+  default     = []
+}
+
+variable "allowed_methods" {
+  type        = list(string)
+  description = "Controls which HTTP methods CloudFront processes and forwards to your Amazon S3 bucket or your custom origin"
+}
+
+variable "cached_methods" {
+  type        = list(string)
+  description = "Controls whether CloudFront caches the response to requests using the specified HTTP methods"
+}
+
+variable "cache_policy_id" {
+  type        = string
+  description = "The unique identifier of the cache policy that is attached to the cache behavior"
+  default     = ""
+}
+
+variable "comment" {
+  type        = string
+  description = "Any comments you want to include about the distribution"
+  default     = ""
+}
+
+variable "compress" {
+  type        = bool
+  description = "Whether you want CloudFront to automatically compress content for web requests that include `Accept-Encoding: gzip` in the request header"
+  default     = false
+}
+
+variable "custom_error_response" {
+  type = list(object({
+    error_code            = string
+    error_caching_min_ttl = number
+    response_code         = string
+    response_page_path    = string
+  }))
+  description = "One or more custom error response elements (multiples allowed)"
+}
+
+variable "default_ttl" {
+  type        = number
+  description = ""
+  default     = 3600
+}
+
+variable "default_root_object" {
+  type        = string
+  description = "The object that you want CloudFront to return (for example, index.html) when an end user requests the root URL"
+  default     = ""
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Whether the distribution is enabled to accept end user requests for content"
+  default     = true
+}
+
+variable "field_level_encryption_id" {
+  type        = string
+  description = "Field level encryption configuration ID"
+  default     = ""
+}
+
+variable "forwarded_values" {
+  type = object({
+    query_string            = bool
+    headers                 = optional(list(string))
+    query_string_cache_keys = optional(list(string))
+    cookies = object({
+      forward           = string
+      whitelisted_names = optional(list(string))
+    })
+  })
+  description = "The forwarded values configuration that specifies how CloudFront handles query strings, cookies and headers (maximum one)"
+  default = {
+    query_string = false
+    cookies = {
+      forward = "none"
+    }
+  }
+}
+
+variable "function_association" {
+  type = list(object({
+    event_type = string
+    lambda_arn = string
+  }))
+  description = "A config block that triggers a cloudfront function with specific actions (maximum 2)"
+  default     = []
+  validation {
+    condition     = length(var.function_association) <= 2
+    error_message = "There can be a maximum 2 function_association blocks"
+  }
+}
+
+variable "http_version" {
+  type        = string
+  description = "The maximum HTTP version to support on the distribution"
+  default     = "http2"
+  validation {
+    condition     = contains(["http1.1", "http2", "http2and3", "http3"], var.http_version)
+    error_message = "Allowed values for http_version are `http1.1`, `http2`, `http2and3` and `http3`."
+  }
+}
+
+variable "is_ipv6_enabled" {
+  type        = bool
+  description = "Whether the IPv6 is enabled for the distribution"
+  default     = false
+}
+
+variable "lambda_function_association" {
+  type = list(object({
+    event_type   = string
+    lambda_arn   = string
+    include_body = optional(bool)
+  }))
+  description = "A config block that triggers a lambda function with specific actions (maximum 4)"
+  default     = []
+  validation {
+    condition     = length(var.lambda_function_association) <= 4
+    error_message = "There can be a maximum 4 lambda_function_association blocks"
+  }
+}
+
+variable "logging_config" {
+  type = object({
+    bucket          = string
+    include_cookies = optional(bool)
+    prefix          = optional(string)
+  })
+  description = "The logging configuration that controls how logs are written to your distribution (maximum one)"
+}
+
+variable "max_ttl" {
+  type        = number
+  description = "The maximum amount of time (in seconds) that an object is in a CloudFront cache before CloudFront forwards another request to your origin to determine whether the object has been updated. Only effective in the presence of `Cache-Control max-age`, `Cache-Control s-maxage`, and `Expires headers`."
+  default     = 86400
+}
+
+variable "min_ttl" {
+  type        = number
+  description = "The minimum amount of time that you want objects to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated"
+  default     = 0
+}
+
+variable "ordered_cache_behavior" {
+  type        = list(any)
+  description = "An ordered list of cache behaviors resource for this distribution. List from top to bottom in order of precedence. The topmost cache behavior will have precedence 0"
+  default     = []
+}
+
+variable "origin" {
+  type = list(object({
+    domain_name         = string
+    origin_id           = string
+    connection_attempts = optional(number)
+    connection_timeout  = optional(number)
+    custom_header = optional(list(object({
+      name  = string
+      value = string
+    })))
+    custom_origin_config = optional(object({
+      http_port                = number
+      https_port               = number
+      origin_protocol_policy   = string
+      origin_ssl_protocols     = string
+      origin_keepalive_timeout = optional(number)
+      origin_read_timeout      = optional(number)
+    }))
+    origin_access_control_id = optional(string)
+    origin_path              = optional(string)
+    origin_shield = optional(object({
+      enabled              = bool
+      origin_shield_region = optional(string)
+    }))
+    s3_origin_config = optional(object({
+      origin_access_identity = string
+    }))
+  }))
+  description = "One or more origins for this distribution (multiples allowed)"
+}
+
+variable "origin_group" {
+  type = list(object({
+    origin_id = string
+    failover_criteria = object({
+      status_codes = list(number)
+    })
+    member = list(object({
+      origin_id = string
+    }))
+  }))
+  description = "One or more origin_group for this distribution (multiples allowed)"
+  default     = []
+}
+
+variable "price_class" {
+  type        = string
+  description = "The price class for this distribution"
+  default     = ""
+}
+
+variable "realtime_log_config_arn" {
+  type        = string
+  description = "The ARN of the real-time log configuration that is attached to this cache behavior"
+  default     = ""
+}
+
+variable "response_headers_policy_id" {
+  type        = string
+  description = "The identifier for a response headers policy"
+  default     = ""
+}
+
+variable "restrictions" {
+  type = object({
+    geo_restriction = object({
+      locations        = list(string)
+      restriction_type = string
+    })
+  })
+  description = "The restriction configuration for this distribution (maximum one)"
+}
+
+variable "retain_on_delete" {
+  type        = bool
+  description = "Disables the distribution instead of deleting it when destroying the resource through Terraform. If this is set, the distribution needs to be deleted manually afterwards"
+  default     = false
+}
+
+variable "cloudfront_tags" {
+  type        = object({})
+  description = "Tags for the CloudFront distribution"
+  default     = {}
+}
+
+variable "target_origin_id" {
+  type        = string
+  description = "The value of ID for the origin that you want CloudFront to route requests to when a request matches the path pattern either for a cache behavior or for the default cache behavior"
+}
+
+variable "trusted_key_groups" {
+  type        = list(string)
+  description = "A list of key group IDs that CloudFront can use to validate signed URLs or signed cookies"
+  default     = []
+}
+
+variable "trusted_signers" {
+  type        = list(string)
+  description = "List of AWS account IDs (or `self`) that you want to allow to create signed URLs for private content"
+  default     = []
+}
+
+variable "viewer_certificate" {
+  type = object({
+    acm_certificate_arn            = optional(string)
+    cloudfront_default_certificate = optional(bool)
+    iam_certificate_id             = optional(string)
+    minimum_protocol_version       = optional(string)
+    ssl_support_method             = optional(string)
+  })
+  description = "The SSL configuration for this distribution (maximum one)."
+  default     = {}
+}
+
+variable "viewer_protocol_policy" {
+  type        = string
+  description = "Use this element to specify the protocol that users can use to access the files in the origin specified by TargetOriginId when a request matches the path pattern in PathPattern"
+  validation {
+    condition     = contains(["allow-all", "https-only", "redirect-to-https"], var.viewer_protocol_policy)
+    error_message = "The value for viewer control policy should be `allow-all`, `https-only` or `redirect-to-https`."
+  }
+}
+
+variable "wait_for_deployment" {
+  type        = bool
+  description = "If enabled, the resource will wait for the distribution status to change from `InProgress` to `Deployed`. Setting this to `false` will skip the process"
+  default     = true
+}
+
+variable "web_acl_id" {
+  type        = string
+  description = "A unique identifier that specifies the AWS WAF web ACL, if any, to associate with this distribution"
+}
