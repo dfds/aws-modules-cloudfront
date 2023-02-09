9feb7b77e220f883de5f066bc341677f8b7c4235
Aleksandra Korczak <alekorc@dfds.com>
2023-02-09 14:44:56 +0100

    Add cloudfront_arn output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "domain_name" {
   value       = aws_cloudfront_distribution.this.domain_name
   description = "Domain name"
 }
+
+output "cloudfront_arn" {
+  value       = aws_cloudfront_distribution.this.arn
+  description = "ARN of the cloudfront distribution"
+}
